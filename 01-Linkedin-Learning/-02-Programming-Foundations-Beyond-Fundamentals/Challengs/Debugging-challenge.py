8e8ba1062d64edabc9f64b3bdaeb7a40039b9b99
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2022-06-26 14:22:20 +0200

    Update Debugging-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Debugging-challenge.py
@@ -1,1 +1,23 @@
+# def plant_recommendation(care):
+#    if care = 'low':    # ==
+#        print('alone')
+#    elif care == 'medium':
+#        print('pothos')
+#    elif care == 'medium':  # 'high'
+#        print('orchid')
+#
+#plant_rec('low')    # plant_recommendation
+#plant_recommendation('medium')
+#plant_recommendation('high')
 
+def plant_recommendation(care):
+    if care == 'low':    
+        print('alone')
+    elif care == 'medium':
+        print('pothos')
+    elif care == 'high':  
+        print('orchid')
+
+plant_recommendation('low')    
+plant_recommendation('medium')
+plant_recommendation('high')
