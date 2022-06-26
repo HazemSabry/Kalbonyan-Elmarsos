b7ea63c0e8de8ef441191ed4242743fdb91355b0
HazemSabry <89419013+HazemSabry@users.noreply.github.com>
2022-06-26 14:01:29 +0200

    Update Working-with-collections-challenge.py

--- 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
+++ 01-Linkedin-Learning/-02-Programming-Foundations-Beyond-Fundamentals/Challengs/Working-with-collections-challenge.py
@@ -1,1 +1,14 @@
+stars = ['Sol','Alpha Centauri','Barnard','Wolf 359']
 
+peak = {
+    'African':'Kilimanjaro',
+    'Antarctica':'Vinson',
+    'Australian':'Puncak Jaya',
+    'Eurasian':'Everest',
+    'North_American':'Denali',
+    'Pacific':'Mauna Kea',
+    'South_American':'Aconcagua'
+}
+
+print(stars[3])
+print(peak['Pacific'])
